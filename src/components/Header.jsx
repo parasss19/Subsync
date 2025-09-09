52113543ce914d8afacaa3c1ec2658ce6f0b9865
Paras <mehtaparas1901@gmail.com>
2025-09-09 17:42:12 +0530

    header updated

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,49 +1,53 @@
-import { IoHomeOutline } from "react-icons/io5";
 import { useNavigate } from 'react-router-dom';
-import logoIcon from '../assets/logoIcon.svg';
+import { CalendarSync } from 'lucide-react';
 
 const Header = () => {
   const navigate = useNavigate();
 
+  const handleScroll = () => {
+    const section = document.getElementById("howToUse");
+    if (section) {
+      section.scrollIntoView({ behavior: "smooth" });
+    }
+  };
+
+
   return (
-    <div className="sticky top-5 z-10 mt-5 mb-4 py-2 px-4 rounded-xl bg-[#212121] flex items-center justify-between border border-[#353434]">
-      
+    <div className="sticky top-5 z-20 mt-5 mb-4 py-[5px] px-3 rounded-xl bg-[#212121] flex items-center justify-between border border-[#353434]">
       {/* Left - Logo */}
       <div
         onClick={() => navigate("/")}
-        style={{ fontFamily: "Instrument Serif, serif" }}
-        className="flex gap-1 items-center justify-center cursor-pointer italic text-lg font-medium px-[8px] py-1.5 text-white "
+        className="font_instrument flex gap-2 items-center justify-center cursor-pointer italic text-lg font-medium text-white "
       >
-        <img src={logoIcon} alt="logo image" />
-        Instantly
+        <CalendarSync/> 
+        <span className="font-medium tracking-wider text-xl">Subsync</span> 
       </div>
 
       {/* Right */}
-      <div className="flex gap-3 justify-center items-center">
+      <div className="flex gap-5 justify-center items-center">
         {/* How to use */}
-          <div className="flex items-center gap-1 text-gray-400 cursor-pointer">
-            <div
-              onClick={() => navigate("#")}
-              className="relative group rounded-full hover:bg-[#2b2b2b] hover:text-gray-200 px-1.5 rounded-fdivl py-1.5"
-            >
-              <span className="text-2xl">🧑🏻‍💻</span>            
-              <span className="absolute bottom-full mb-0.5 bg-[#484848] text-white font-[poppins] text-xs rounded px-2 py-1 left-1/2 -translate-x-1/2 translate-y-1 opacity-0 group-hover:opacity-100 group-hover:translate-y-0 transition-all duration-300 ease-in-out whitespace-nowrap">How to use</span>
-            </div>
-          </div>
-
-        {/* How to install btn */}
-        <div className="flex gap-3 items-center ">
+        <div className="flex items-center gap-1 text-gray-400 cursor-pointer">
           <div
-            onClick={() => navigate("/contact")}
-            className="rounded-[6px] bg-[#383838] active:scale-95 transition-transform duration-150"
+            onClick={handleScroll}
+            className="relative group rounded-full hover:bg-[#2b2b2b] hover:text-gray-200 px-1.5 rounded-fdivl py-1.5"
           >
-            <button
-              style={{ fontFamily: "Instrument Serif, serif" }}
-              className="tracking-wider cursor-pointer text-[18px] font-medium px-[8px] py-1.5 text-yellow-200 font-geist"
-            >
-              Install
-            </button>
+            <span className="text-xl sm:text-3xl">🧑🏻‍💻</span>
+            <span className="absolute bottom-full mb-0.5 bg-[#484848] text-white font-[poppins] text-xs rounded px-2 py-1 left-1/2 -translate-x-1/2 translate-y-1 opacity-0 group-hover:opacity-100 group-hover:translate-y-0 transition-all duration-300 ease-in-out whitespace-nowrap">
+              How to use
+            </span>
           </div>
+        </div>
+
+        {/* install btn */}
+        <div className="">
+          <button
+            onClick={() => navigate("/auth")}
+            className="cursor-pointer bg-black/60 text-yellow-300 text-sm sm:text-lg px-2 py-1.5 sm:px-4 sm:py-1.5 rounded-lg shadow-md transition-transform duration-150 active:scale-95"
+          >
+            <span className="font_instrument tracking-wider flex items-center font-medium justify-center gap-1">
+              Install
+            </span>
+          </button>
         </div>
       </div>
 
