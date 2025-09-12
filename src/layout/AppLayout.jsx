d500be1c2bce7fbe25e9c8e5866d4904efcd2663
Paras <mehtaparas1901@gmail.com>
2025-09-12 16:55:10 +0530

    layout updated

--- src/layout/AppLayout.jsx
+++ src/layout/AppLayout.jsx
@@ -2,7 +2,6 @@ import { Outlet } from "react-router-dom";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 import ScrollToTop from '../components/ScrollToTop'
-
 const AppLayout = () => {
   return (
     <div className="min-h-screen w-full relative bg-black">
