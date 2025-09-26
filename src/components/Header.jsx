188520866acc91ebae1094b57704c8bc6d29e13e
Paras <mehtaparas1901@gmail.com>
2025-09-26 13:18:33 +0530

    header links updated

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -26,17 +26,10 @@ const Header = () => {
 
       {/* Right */}
       <div className="flex gap-5 justify-center items-center">
-        {/* How to use */}
-        <div className="flex items-center gap-1 text-gray-400 cursor-pointer">
-          <div
-            onClick={handleScroll}
-            className="relative group rounded-full hover:bg-[#2b2b2b] hover:text-gray-200 px-1.5 rounded-fdivl py-1.5"
-          >
-            <span className="text-xl sm:text-3xl">🧑🏻‍💻</span>
-            <span className="absolute bottom-full mb-0.5 bg-[#484848] text-white font-[poppins] text-xs rounded px-2 py-1 left-1/2 -translate-x-1/2 translate-y-1 opacity-0 group-hover:opacity-100 group-hover:translate-y-0 transition-all duration-300 ease-in-out whitespace-nowrap">
-              How to use
-            </span>
-          </div>
+        {/* Price Page */}
+        <div className="flex items-center gap-2 sm:gap-4 text-gray-400 cursor-pointer font_instrument">
+          <span onClick={() => navigate('/use')} className=" text-white font-semibold font-[Geist] text-xs sm:text-lg bg-black/20 rounded-xl px-3 py-1 hover:scale-110 duration-200">Use</span>
+          <span onClick={() => navigate('/price')} className=" text-white font-semibold font-[Geist] text-xs sm:text-lg bg-black/20 rounded-xl px-3 py-1 hover:scale-110 duration-200">Price</span>
         </div>
 
         {/* install btn */}
